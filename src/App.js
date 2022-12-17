e89de279ed79a7d7efbe27c2a2e153f450d95261
Vartik <vartikjain45@gmail.com>
2022-12-17 17:56:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+function App  () {
+
+  const [val, setval] = useState('');
+  const [res,setres] = useState('');
+
+const update = value =>{
+   setval(val + value);
 }
 
-export default App;
+const calcvalue=()=>{
+
+  setres(eval(val))
+  setval('')
+ 
+}
+const clr=()=>{
+ 
+  setres('')
+  setval('')
+}
+
+  return (
+    <div className="container">
+    <h1>Calculator</h1>
+
+    <div className="calculator">
+        
+        <table  id='tbl'>
+        
+          <tr>
+            <td colspan='4' id='tr1'>{res ? <span>{res}</span> : val}</td>
+            
+          </tr>
+            <tr>
+                <td className='td1' onClick={()=>update('(')}>(</td>
+                <td className='td1' onClick={()=>update(')')}>)</td>
+                <td className='td1' onClick={clr}>C</td>
+                <td className='td1' onClick={()=>update('%')}>%</td>
+            </tr>
+            <tr >
+                <td className='td1' onClick={()=>update('7')}>7</td>
+                <td className='td1' onClick={()=>update('8')}>8</td>
+                <td className='td1' onClick={()=>update('9')}>9</td>
+                <td className='td1' onClick={()=>update('*')}>X</td>
+            </tr>
+            <tr>
+                <td className='td1' onClick={()=>update('4')}>4</td>
+                <td className='td1' onClick={()=>update('5')}>5</td>
+                <td className='td1' onClick={()=>update('6')}>6</td>
+                <td className='td1'  onClick={()=>update('-')}>-</td>
+            </tr>
+            <tr>
+                <td className='td1'  onClick={()=>update('1')}>1</td>
+                <td className='td1' onClick={()=>update('2')}>2</td>
+                <td className='td1' onClick={()=>update('3')}>3</td>
+                <td className='td1' onClick={()=>update('+')}>+</td>
+            </tr>
+            <tr>
+                <td className='td1' onClick={()=>update('0')}>0</td>
+                <td className='td1' onClick={()=>update('.')}>.</td>
+                <td className='td1' onClick={()=>update('/')}>/</td>
+                <td className='td1' onClick={calcvalue}>=</td>
+            </tr>
+        </table>
+    </div>
+</div>
+  )
+  
+}
+{/*  */}
+
+export default App
